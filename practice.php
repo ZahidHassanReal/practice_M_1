9a450035b21e360cfc2b6d51c27bdf718b1c98a4
Real <hassanzahidreal@gmail.com>
2023-10-13 12:39:22 +0600

    callback_final

--- practice.php
+++ practice.php
@@ -1,36 +1,27 @@
 <?php
 
+$books = [
 
+    ["id"=>"1", "title"=>"javascript", "author"=>"manik chad"],
+    ["id"=>"2", "title"=>"php", "author"=>"manik. kalam. ajad"],
+    ["id"=>"3", "title"=>"java", "author"=>"manik"],
+    ["id"=>"4", "title"=>"node", "author"=>"maninka miya avinue"],
+    ["id"=>"5", "title"=>"laravel", "author"=>" AB"],
+    ["id"=>"6", "title"=>"javascript 2", "author"=>"manik chad"],
 
-$people = [
-
-    ['name'=>'A','gender'=>'male'],
-    ['name'=>'B','gender'=>'female'],
-    ['name'=>'C','gender'=>'female'],
-    ['name'=>'D','gender'=>'male'],
-    ['name'=>'E','gender'=>'female'],
 ];
 
-function isMale($person){
-    if($person['gender']=='male'){
-     return true;
-    }
-
-    return false;
-
-
-
+function sortByAuthorName($book1, $book2){
+    if(strlen($book1['author']) > strlen($book2['author'])){
+        return -1;
+    }elseif(strlen($book1['author']) < strlen($book2['author'])){
+        return 1;
+}else{
+    return 0;
 }
-function isFemale($person){
-    if($person['gender']=='female'){
-     return true;
-    }
-
-    return false;
 
 }
 
-$males = array_filter($people, "isMale");
+usort ($books, "sortByAuthorName");
 
-
-print_r ($males);
+print_r($books);
